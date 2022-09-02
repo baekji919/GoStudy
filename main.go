4b22045422e7c62f7f814d259ac2dc76c7d027dd
jihyun <jamie919@beautifulstore.org>
2022-09-02 16:24:58 +0900

    http 호출

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -13,9 +17,18 @@ func main()  {
 
 	db, err := sql.Open("mysql", ConnectionString)
 	if err != nil {
+		marshal, err := json.Marshal(map[string]string{"text": fmt.Sprintf("%+v", err)})
+		if err != nil {
+			log.Fatal("Json Marshal Error: ",err)
+		}
+		requestBody := bytes.NewBuffer(marshal)
+
+		resp, err := http.Post("https://hooks.slack.com/", "application/json", requestBody)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
 		fmt.Print("Error : ", err)
 	}
 	db.Close()
-
-	fmt.Println("Success")
 }
